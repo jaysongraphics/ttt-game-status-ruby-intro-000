c88a8c533038342acd7c383b8478664df4eed188
pleasant-tree-5cd4 <jaysongraphics@gmail.com>
2021-03-31 16:06:33 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,7 +14,7 @@ WIN_COMBINATIONS = [
 [6,4,2] # right_diagonal
 ]
 
-def won?(array)
+def won?(board)
   WIN_COMBINATIONS.find do |win_combination|
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
