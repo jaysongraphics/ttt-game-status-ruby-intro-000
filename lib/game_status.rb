adc318f9fe0dd833ddc51b383772a06ecac9ebf0
pleasant-tree-5cd4 <jaysongraphics@gmail.com>
2021-03-31 15:34:12 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,13 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-def WIN_COMBINATIONS
+def WIN_COMBINATIONS = = [
+[0,1,2], # top_row
+[3,4,5], # middle_row
+[6,7,8], # bottom_row
+[0,3,6], # left_column
+[1,4,7], # center_column
+[2,5,8], # right_column
+[0,4,8], # left_diagonal
+[6,4,2] # right_diagonal
+]
