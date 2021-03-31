0d8258f56bd0f0b4e0215db98d7ef7c8be4f5d1d
pleasant-tree-5cd4 <jaysongraphics@gmail.com>
2021-03-31 15:58:59 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,8 +16,11 @@ WIN_COMBINATIONS = [
 
 def won?(board)
     WIN_COMBINATIONS.each do |combinations|
-      if combinations.all? {|combination| combination == 'X' or combination == 'O'}
-
+      position_1 = board[win_index_1] # value of board at win_index_1
+      position_2 = board[win_index_2] # value of board at win_index_2
+      position_3 = board[win_index_3] # value of board at win_index_3
+      position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
+    end
       end
 
 def full?(board)
